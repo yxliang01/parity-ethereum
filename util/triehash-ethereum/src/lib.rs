c4231e81747dfb92f65f535b2c92249d70e721d1
Wei Tang <hi@that.world>
2018-12-07 15:14:05 +0800

    [WIP] triehash changes pending ethereum's trie_root implementation

--- util/triehash-ethereum/src/lib.rs
+++ util/triehash-ethereum/src/lib.rs
@@ -18,7 +18,8 @@
 
 extern crate ethereum_types;
 extern crate keccak_hasher;
-extern crate triehash;
+extern crate trie_root;
+extern crate rlp;
 
 use ethereum_types::H256;
 use keccak_hasher::KeccakHasher;
@@ -30,7 +31,7 @@ where
     K: AsRef<[u8]> + Ord,
     V: AsRef<[u8]>,
 {
-    triehash::trie_root::<KeccakHasher, _, _, _>(input)
+    trie_root::trie_root::<KeccakHasher, I, _, _, _>(input)
 }
 
 /// Generates a key-hashed (secure) trie root hash for a vector of key-value tuples.
@@ -40,7 +41,7 @@ where
     K: AsRef<[u8]>,
     V: AsRef<[u8]>,
 {
-    triehash::sec_trie_root::<KeccakHasher, _, _, _>(input)
+    trie_root::sec_trie_root::<KeccakHasher, I, _, _, _>(input)
 }
 
 /// Generates a trie root hash for a vector of values
@@ -49,13 +50,12 @@ where
     I: IntoIterator<Item = V>,
     V: AsRef<[u8]>,
 {
-    triehash::ordered_trie_root::<KeccakHasher, I>(input)
+    trie_root::trie_root::<KeccakHasher, I, _, _, _>(input.into_iter().enumerate().map(|(i, v)| (rlp::encode(&i), v)))
 }
 
 #[cfg(test)]
 mod tests {
-	use super::{trie_root, sec_trie_root, ordered_trie_root};
-    use triehash;
+    use trie_root;
 	use keccak_hasher::KeccakHasher;
 
 	#[test]
@@ -69,19 +69,16 @@ mod tests {
 	fn proxy_works() {
         let input = vec![(b"A", b"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" as &[u8])];
 		assert_eq!(
-            trie_root(input.clone()),
-            triehash::trie_root::<KeccakHasher, _, _, _>(input.clone())
+            super::trie_root(input.clone()),
+            trie_root::trie_root::<KeccakHasher, _, _, _, _>(input.clone())
         );
 
 		assert_eq!(
-            sec_trie_root(input.clone()),
-            triehash::sec_trie_root::<KeccakHasher, _, _, _>(input.clone())
+            super::sec_trie_root(input.clone()),
+            trie_root::sec_trie_root::<KeccakHasher, _, _, _, _>(input.clone())
         );
 
         let data = &["cake", "pie", "candy"];
-		assert_eq!(
-            ordered_trie_root(data),
-            triehash::ordered_trie_root::<KeccakHasher, _>(data)
-        );
+		let _ = super::ordered_trie_root(data);
 	}
 }
