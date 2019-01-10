53dada76c961aaad66479174b9fedeb971d289a6
Denis P <pisarevden@gmail.com>
2019-01-10 17:01:52 +0100

    audit is being installed in Dockerfile

--- scripts/gitlab/cargo-audit.sh
+++ scripts/gitlab/cargo-audit.sh
@@ -3,9 +3,5 @@
 set -e # fail on any error
 set -u # treat unset variables as error
 
-<<<<<<< HEAD
 # CARGO_TARGET_DIR=./target cargo +stable install cargo-audit --force
-=======
-cargo install cargo-audit
->>>>>>> master
 cargo audit
