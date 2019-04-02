3cc2d4fc47c82f9fede1124389df4ebc9e6e8a11
Xiao Liang <yxliang01@users.noreply.github.com>
2019-04-02 14:03:02 +0000

    wip

--- ethcore/src/trace/mod.rs
+++ ethcore/src/trace/mod.rs
@@ -86,7 +86,7 @@ pub trait VMTracer: Send {
 	fn trace_prepare_execute(&mut self, _pc: usize, _instruction: u8, _gas_cost: U256, _mem_written: Option<(usize, usize)>, _store_written: Option<(U256, U256)>) {}
 
 	/// Trace the finalised execution of a single valid instruction.
-	fn trace_executed(&mut self, _gas_used: U256, _stack_push: &[U256], _mem: &[u8]) {}
+	fn trace_executed(&mut self, _gas_used: U256, _stack_push: &[U256], _stack_pop: &[U256], _mem: &[u8]) {}
 
 	/// Spawn subtracer which will be used to trace deeper levels of execution.
 	fn prepare_subtrace(&mut self, _code: &[u8]) {}
