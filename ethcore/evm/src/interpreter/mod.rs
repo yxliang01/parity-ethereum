389fc462dbdd226b42c43913e6db41419e57d3cf
Xiao Liang <yxliang01@users.noreply.github.com>
2019-04-02 17:59:38 +0000

    wip

--- ethcore/evm/src/interpreter/mod.rs
+++ ethcore/evm/src/interpreter/mod.rs
@@ -355,7 +355,9 @@ impl<Cost: CostType> Interpreter<Cost> {
 
 				evm_debug!({ self.informant.before_instruction(self.reader.position, instruction, info, &self.gasometer.as_mut().expect(GASOMETER_PROOF).current_gas, &self.stack) });
 
-				self.last_stack_pop = self.stack.peek_top(info.args).reverse().to_vec();
+				let reversedStackPoped = self.stack.peek_top(info.args);
+				reversedStackPoped.reverse();
+				self.last_stack_pop = reversedStackPoped.to_vec();
 
 				// Execute instruction
 				let current_gas = self.gasometer.as_mut().expect(GASOMETER_PROOF).current_gas;
